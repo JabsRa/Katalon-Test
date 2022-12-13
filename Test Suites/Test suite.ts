b27d60a0a8f60cab9a49cdc0c8ed1389232ecbd0
JabsRa <80768185+JabsRa@users.noreply.github.com>
2022-12-13 15:04:44 +0700

    change

---
+++ Test Suites/Test suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>14781921-5830-4266-8980-92b096d26e6c</testSuiteGuid>
+   <testCaseLink>
+      <guid>e5a64458-0d5a-4904-b3d0-9e475c81aca7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
